257bc1404f0b01bc12f21c7ef9c0ba1c46b04945
Jimmy <vaugh@hotmail.se>
2024-02-28 10:51:08 +0100

    Print active and stale app registations

--- StaleAppRegistrations/Program.cs
+++ StaleAppRegistrations/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using System.Text.Json;
+using WojciechMikołajewicz.CsvReader;
 
-Console.WriteLine("Hello, World!");
+var signInFile = "..\\..\\..\\..\\data\\SignIns.json";
+var appRegistrationFile = "..\\..\\..\\..\\data\\AppRegistrationList.csv";
+
+var signIns = JsonSerializer.Deserialize<List<SignIn>>(File.ReadAllText(signInFile));
+
+using var textReader = new StreamReader(appRegistrationFile, Encoding.UTF8, true);
+using var csvDeserializer = new CsvDeserializer<AppRegistration>(textReader);
+var appRegistrations = await csvDeserializer.ReadAllToListAsync();
+
+Console.WriteLine($"signIns count:\t\t{signIns.Count}");
+Console.WriteLine($"appRegistrations count:\t{appRegistrations.Count}");
+
+Console.WriteLine("");
+
+var activeAppRegistrations = appRegistrations.Where(appRegistration => signIns.Any(signIn => signIn.appId == appRegistration.appID));
+var staleAppRegistrations = appRegistrations.Where(appRegistration => signIns.All(signIn => signIn.appId != appRegistration.appID));
+
+Console.WriteLine("Active app registrations");
+Console.WriteLine("========================");
+Console.WriteLine("App id\t\t\t\t\tName");
+foreach (var appRegistration in activeAppRegistrations)
+    Console.WriteLine($"{appRegistration.appID}\t{appRegistration.displayName}");
+
+Console.WriteLine("");
+Console.WriteLine("Stale app registrations");
+Console.WriteLine("=======================");
+Console.WriteLine("App id\t\t\t\t\tName");
+foreach (var appRegistration in staleAppRegistrations)
+    Console.WriteLine($"{appRegistration.appID}\t{appRegistration.displayName}");
+
+public class SignIn
+{
+    public string appId { get; set; }
+}
+
+public class AppRegistration
+{
+    public string displayName { get; set; }
+    public string appID { get; set; }
+}
+
+
